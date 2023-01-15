b74c9348949d3775aadeeddb362ef01b45054161
Daniel Ruiz <danielruimar@gmail.com>
2023-01-15 10:20:12 +0100

    solved exercise 14

--- main.js
+++ main.js
@@ -503,9 +503,10 @@ let sudokuIntro = [
   ];
 
 
+  
 
 function sudoku (array){
-    
+
     let serieRequired = [1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 9, 9, 9, 9, 9, 9, 9, 9, 9];
     let serieRequiredtoString = serieRequired.toString();
     let newArray =[];
